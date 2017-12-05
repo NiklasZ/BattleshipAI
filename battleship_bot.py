554955d5bf5eea3794ffc8c5f0939398654b7994
NiklasZ <niklas.zwingenberger@gmail.com>
2017-12-05 13:16:13 +0100

    -Completed basic bot (it now functions). -Finished testing harness for bot.

--- battleship_bot.py
+++ battleship_bot.py
@@ -5,151 +5,193 @@
 #  |____/ \__,_|\__|\__|_|\___||___/_| |_|_| .__/|___/
 #                                          |_|
 #
-botName='Nbot'
+botName='NbotI'
 
 from random import randint, choice
 from collections import namedtuple
 
 import json
 
-# These are the only additional libraries available to you. Uncomment them
-# to use them in your solution.
-#
 import numpy as np   # Base N-dimensional array package
 #import pandas   # Data structures & analysis
 
 #Structs that contain info & heuristic values to choose next move.
 
-#TODO enhance hits with impact on alignment
-#TODO get more info on different move options.
+#TODO remove impact (has no effect)
+#TODO adjust hitting to only bother with directions that make sense (e.g don't fire at positions where the ship cannot exist).
+#TODO perform subset checks on alignments to remove irrelevant choices entirely (useful when going probabilistic)-
 def calculateMove(gameState):
-    oppShips = shipsStillAfloat(gameState)
-    oppBoard = np.array(gameState['OppBoard'])
+
+    gameState['opp_board'] = gameState['OppBoard']
+
+    opp_ships = shipsStillAfloat(gameState)
+    opp_board = np.array(gameState['opp_board'])
+
+    #Deploy ships at start of game.
+    if gameState['Round'] == 0:
+        return deployRandomly(gameState)
+
+
     #If there are hits, try nearby targets.
-    if 'H' in oppBoard:
-        moves = possibleHits(oppBoard,oppShips)
-        sorted_moves = sorted(moves, key=lambda x:x.hitLength)
-        choice = sorted_moves[0]
+    if 'H' in opp_board:
+        moves = possible_hits(opp_board,opp_ships)
+        highest = max(moves, key=lambda x:moves[x])
+        choices = [move for move in moves if moves[move] == moves[highest]]
+        y,x = choice(choices)
+
     #If not, find possible targets from the grid.
     else:
-        moves = possibleTargets(oppBoard, oppShips)
-        sorted_align = sorted(moves, key=lambda x:x.alignments)
-        sorted_impact = sorted(moves, key=lambda x:x.impact)
-        choice = sorted_impact[0]
+        moves = possible_targets(opp_board, opp_ships)
+        align_highest = max(moves, key=lambda x:x['alignment'])['alignment']
+        impact_highest = max(moves, key=lambda x:x['impact'])['impact']
 
+        align_choices = [move for move in moves if move['alignment'] == align_highest]
+        impact_choices = [move for move in moves if move['impact'] == impact_highest]
+
+        print('Targeting...')
+        print('choices:',impact_choices)
+        if(impact_choices != impact_choices):
+            print('align recommends different choice:',align_choices)
+        coord = choice(impact_choices)
+        y = coord['y']
+        x = coord['x']
+
+    print("Firing at:",(y,x))
+    print("Position on board:",translateMove(y,x))
     return translateMove(y,x)
 
 #Find locations adjacent to possible hits and give preference to
 #long hit sequences.
-def possibleHits(oppBoard, oppShips):
-    hits = []
-    vertVisited = np.zeros(oppBoard.shape, dtype=bool)
-    horiVisited = np.zeros(oppBoard.shape, dtype=bool)
-    for (y,x) in np.ndenumerate(oppBoard):
-        if oppBoard[y,x] == 'H':
+def possible_hits(opp_board, opp_ships):
+    hits = {}
+    vert_visited = np.zeros((len(opp_board),len(opp_board[0])), dtype=bool)
+    horz_visited = np.zeros((len(opp_board),len(opp_board[0])), dtype=bool)
+    for idx,val in np.ndenumerate(opp_board):
+        y = idx[0]
+        x = idx[1]
+        if opp_board[y][x] == 'H':
             #Search vertically
-            if not vertVisited[y,x]:
-                vertVisited[y,x] = True
-                vertLength = 1
+            if not vert_visited[y,x]:
+                vert_visited[y,x] = True
+                vert_length = 1
                 top = bottom = None
 
                 #Search above
-                for i in range(1,y):
-                    if oppBoard[y-i,x] == 'H':
-                        vertVisited[y-i,x] = True
-                        vertLength += 1
+                i=1
+                while(y-i >= 0):
+                    if opp_board[y-i][x] == 'H':
+                        vert_visited[y-i,x] = True
+                        vert_length += 1
                     else:
-                        top = (y-i,x)
+                        if opp_board[y-i][x] == '':
+                            top = (y-i,x)
                         break
+                    i+=1
 
                 #Search below
-                for i in range(1,oppBoard.shape[0]-y):
-                    if oppBoard[y+i,x] == 'H':
-                        vertVisited[y+i,x] = True
-                        vertLength += 1
+                i=1
+                while(y+i < len(opp_board)):
+                    if opp_board[y+i][x] == 'H':
+                        vert_visited[y+i,x] = True
+                        vert_length += 1
                     else:
-                        bottom = (y+i,x)
+                        if opp_board[y+i][x] == '':
+                            bottom = (y+i,x)
                         break
+                    i+=1
 
                 if top:
-                    hits.append(makeHit(top[0],top[1],vertLength))
+                    add_to_hits(hits, top, vert_length)
                 if bottom:
-                    hits.append(makeHit(bottom[0],bottom[1],vertLength))
+                    add_to_hits(hits, bottom, vert_length)
 
             #Search horizontally
-            if not horzVisited[y,x]:
-                horzVisited[y,x] = True
-                horzLength = 1
+            if not horz_visited[y,x]:
+                horz_visited[y,x] = True
+                horz_length = 1
                 left = right = None
 
                 #Search left
-                for j in range(1,x):
-                    if oppBoard[y,x-j] == 'H':
-                        horzLength[y,x-j] = True
-                        horzLength += 1
+                j=1
+                while(x-j >= 0):
+                    if opp_board[y][x-j] == 'H':
+                        horz_length[y,x-j] = True
+                        horz_length += 1
                     else:
-                        left = (y,x-j)
+                        if opp_board[y][x-j] == '':
+                            left = (y,x-j)
                         break
+                    j+=1
 
                 #Search right
-                for i in range(1,oppBoard.shape[0]-y):
-                    if oppBoard[y,x+j] == 'H':
-                        horzVisited[y,x+j] = True
-                        horzLength += 1
+                j=1
+                while(x+j < len(opp_board[0])):
+                    if opp_board[y][x+j] == 'H':
+                        horz_visited[y,x+j] = True
+                        horz_length += 1
                     else:
-                        right = (y,x+j)
+                        if opp_board[y][x+j] == '':
+                            right = (y,x+j)
                         break
+                    j+=1
 
                 if left:
-                    hits.append(makeHit(left[0],left[1],horzLength))
+                    add_to_hits(hits, left, horz_length)
                 if right:
-                    hits.append(makeHit(right[0],right[1],horzLength))
+                    add_to_hits(hits, right, horz_length)
 
     return hits
 
+#Helper method for adding hits. Aggregates points where multiple Hs meet.
+def add_to_hits(hits,coord,seqLength):
+    if coord not in hits:
+        hits[coord] = seqLength
+    else:
+        hits[coord] += seqLength
+
 #Go through each valid tile and attempt a shot. Observe how much this reduces possible
 #alignments and return each move.
-def possibleTargets(oppBoard, oppShips):
-    defAlignment, totalAlignments = possibleAlignments(oppBoard, oppShips)
+def possible_targets(opp_board, opp_ships):
+    def_alignment, total_alignments = possible_alignments(opp_board, opp_ships)
 
     #Get all non-zero possible alignements and their indices.
-    targets = [makeTarget(y,x,val,0) for y,row in enumerate(defAlignment) for x,val  in enumerate(row) if val > 0]
+    targets = [make_target(y,x,val,0) for y,row in enumerate(def_alignment) for x,val  in enumerate(row) if val > 0]
 
     #Try shooting at a coordinate to see how much it reduces the total alignments.
     for target in targets:
-        oppBoard[target['y']][target['x']] = 'T'
-        target['impact'] = totalAlignments - possibleAlignments(oppBoard, oppShips)[1]
-        oppBoard[target['y']][target['x']] = ''
+        opp_board[target['y']][target['x']] = 'T'
+        target['impact'] = total_alignments - possible_alignments(opp_board, opp_ships)[1]
+        opp_board[target['y']][target['x']] = ''
 
     return targets
 
 #For each cell, calculate the number of possible alignments and return the results + possibilities.
-def possibleAlignments(oppBoard, oppShips):
-    alignments = np.zeros((len(oppBoard),len(oppBoard[0])), dtype=int)
+def possible_alignments(opp_board, opp_ships):
+    alignments = np.zeros((len(opp_board),len(opp_board[0])), dtype=int)
     #y is the row, x is the column
-    for y in range(0,len(oppBoard)):
-        for x in range(0,len(oppBoard[0])):
-            if oppBoard[y][x] == '':
-                alignments[y,x] = alignmentsIn(y,x, oppBoard, oppShips)
+    for y in range(0,len(opp_board)):
+        for x in range(0,len(opp_board[0])):
+            if opp_board[y][x] == '':
+                alignments[y,x] = alignments_in(y,x, opp_board, opp_ships)
 
     return (alignments, np.sum(alignments))
 
 #Gets number of valid alignments for a position
-def alignmentsIn(y,x, oppBoard, oppShips):
-    validAlignments = 0
-    for shipLength in oppShips:
+def alignments_in(y,x, opp_board, opp_ships):
+    valid_alignments = 0
+    for shipLength in opp_ships:
         for i in range(0,shipLength):
             #Vertical alignment attempts
-            if y-i >= 0 and canDeploy(y-i,x, oppBoard, shipLength, "V"):
-                validAlignments +=1
+            if y-i >= 0 and can_deploy(y-i,x, opp_board, shipLength, "V"):
+                valid_alignments +=1
             #Horizontal alignment attempts
-            if x-i >= 0 and canDeploy(y,x-i, oppBoard, shipLength,"H"):
-                validAlignments += 1
+            if x-i >= 0 and can_deploy(y,x-i, opp_board, shipLength,"H"):
+                valid_alignments += 1
 
-    return validAlignments
+    return valid_alignments
 
 #Ascertains if a given ship can be deployed at a given location
-def canDeploy(i,j,board, length, orientation):
+def can_deploy(i,j,board, length, orientation):
     if orientation == "V":  # If we are trying to place ship vertically
         if i + length - 1 >= len(board):  # If ship doesn't fit within board boundaries
             return False  # Ship not deployed
@@ -165,17 +207,14 @@ def canDeploy(i,j,board, length, orientation):
     return True  # Ship fits
 
 #Helper methods to put variables into a dictionary.
-def makeTarget(y,x,alignment,impact):
+def make_target(y,x,alignment,impact):
     return {'y':y,'x':x,'alignment':alignment,'impact':impact}
-
-def makeHit(y,x,seqLength):
-    return {'y':y,'x':x,'seqLength':seqLength}
 
 # =============================================================================
 # The code below shows a selection of helper functions designed to make the
 # time to understand the environment and to get a game running as short as
 # possible. The code also serves as an example of how to manipulate the myBoard
-# and oppBoard dictionaries that are in gameState.
+# and opp_board dictionaries that are in gameState.
 
 # Deploys all the ships randomly on a blank board
 def deployRandomly(gamestate):
@@ -216,21 +255,6 @@ def deployShip(i, j, board, length, orientation, ship_num):
             board[i][j + l] = str(ship_num)  # Place the ship on the board
     return True  # Ship deployed
 
-# Randomly guesses a location on the board that hasn't already been hit
-def chooseRandomValidTarget(gamestate):
-    valid = False
-    row = None
-    column = None
-    while not valid:  # Keep randomly choosing targets until a valid one is chosen
-        row = randint(0, len(gamestate["MyBoard"]) - 1)  # Randomly pick a row
-        column = randint(0, len(gamestate["MyBoard"][0]) - 1)  # Randomly pick a column
-        if gamestate["OppBoard"][row][column] == "":  # If the target is sea that hasn't already been guessed...
-            valid = True  # ...then the target is valid
-    move = {"Row": chr(row + 65),
-            "Column": (column + 1)}  # Set move equal to the valid target (convert the row to a letter 0->A, 1->B etc.)
-    return move  # Return the move
-
-
 # Returns a list of the lengths of your opponent's ships that haven't been sunk
 def shipsStillAfloat(gamestate):
     afloat = []
@@ -238,27 +262,13 @@ def shipsStillAfloat(gamestate):
     for k in range(len(gamestate["Ships"])):  # For every ship
         afloat.append(gamestate["Ships"][k])  # Add it to the list of afloat ships
         ships_removed.append(False)  # Set its removed from afloat list to false
-    for i in range(len(gamestate["OppBoard"])):
-        for j in range(len(gamestate["OppBoard"][0])):  # For every grid on the board
+    for i in range(len(gamestate["opp_board"])):
+        for j in range(len(gamestate["opp_board"][0])):  # For every grid on the board
             for k in range(len(gamestate["Ships"])):  # For every ship
-                if str(k) in gamestate["OppBoard"][i][j] and not ships_removed[k]:  # If we can see the ship number on our opponent's board and we haven't already removed it from the afloat list
+                if str(k) in gamestate["opp_board"][i][j] and not ships_removed[k]:  # If we can see the ship number on our opponent's board and we haven't already removed it from the afloat list
                     afloat.remove(gamestate["Ships"][k])  # Remove that ship from the afloat list (we can only see an opponent's ship number when the ship has been sunk)
                     ships_removed[k] = True  # Record that we have now removed this ship so we know not to try and remove it again
     return afloat  # Return the list of ships still afloat
-
-
-# Returns a list of cells adjacent to the input cell that are free to be targeted (not including land)
-def selectUntargetedAdjacentCell(row, column, oppBoard):
-    adjacent = []  # List of adjacent cells
-    if row > 0 and oppBoard[row - 1][column] == "":  # If there is a cell above
-        adjacent.append((row - 1, column))  # Add to list of adjacent cells
-    if row < len(oppBoard) - 1 and oppBoard[row + 1][column] == "":  # If there is a cell below
-        adjacent.append((row + 1, column))  # Add to list of adjacent cells
-    if column > 0 and oppBoard[row][column - 1] == "":  # If there is a cell left
-        adjacent.append((row, column - 1))  # Add to list of adjacent cells
-    if column < len(oppBoard[0]) - 1 and oppBoard[row][column + 1] == "":  # If there is a cell right
-        adjacent.append((row, column + 1))  # Add to list of adjacent cells
-    return adjacent
 
 
 # Given a valid coordinate on the board returns it as a correctly formatted move
--- battleship_test.py
+++ battleship_test.py
@@ -9,24 +9,24 @@ class TestDeployment(unittest.TestCase):
 		board = [['','2','2'],
 		['','1',''],
 		['','1','']]
-		self.assertFalse(bs.canDeploy(0,0,board,2,'H'))
-		self.assertFalse(bs.canDeploy(0,0,board,4,'V'))
-		self.assertFalse(bs.canDeploy(1,1,board,1,'V'))
-		self.assertFalse(bs.canDeploy(1,1,board,1,'H'))
-		self.assertFalse(bs.canDeploy(2,2,board,2,'V'))
-		self.assertFalse(bs.canDeploy(2,2,board,2,'H'))
+		self.assertFalse(bs.can_deploy(0,0,board,2,'H'))
+		self.assertFalse(bs.can_deploy(0,0,board,4,'V'))
+		self.assertFalse(bs.can_deploy(1,1,board,1,'V'))
+		self.assertFalse(bs.can_deploy(1,1,board,1,'H'))
+		self.assertFalse(bs.can_deploy(2,2,board,2,'V'))
+		self.assertFalse(bs.can_deploy(2,2,board,2,'H'))
 
 	#Check if it will accept valid deployments
 	def test_deployment_accept(self):
 		board = [['','','',''],
 		['','1','',''],
 		['','1','','']]
-		self.assertTrue(bs.canDeploy(0,0,board,2,'H'))
-		self.assertTrue(bs.canDeploy(0,1,board,2,'H'))
-		self.assertTrue(bs.canDeploy(0,0,board,4,'H'))
-		self.assertTrue(bs.canDeploy(0,0,board,3,'V'))
-		self.assertTrue(bs.canDeploy(0,2,board,3,'V'))
-		self.assertTrue(bs.canDeploy(0,3,board,3,'V'))
+		self.assertTrue(bs.can_deploy(0,0,board,2,'H'))
+		self.assertTrue(bs.can_deploy(0,1,board,2,'H'))
+		self.assertTrue(bs.can_deploy(0,0,board,4,'H'))
+		self.assertTrue(bs.can_deploy(0,0,board,3,'V'))
+		self.assertTrue(bs.can_deploy(0,2,board,3,'V'))
+		self.assertTrue(bs.can_deploy(0,3,board,3,'V'))
 
 class TestAlignments(unittest.TestCase):
 	#Check if the method gets the right number of alignments given ships and board.
@@ -37,9 +37,9 @@ class TestAlignments(unittest.TestCase):
 		['L','','M','',''],
 		['','M','','',''],
 		['','M','','','']]
-		self.assertEqual(bs.alignmentsIn(0,0,board,ships),3)
-		self.assertEqual(bs.alignmentsIn(0,4,board,ships),4)
-		self.assertEqual(bs.alignmentsIn(2,1,board,ships),0)
+		self.assertEqual(bs.alignments_in(0,0,board,ships),3)
+		self.assertEqual(bs.alignments_in(0,4,board,ships),4)
+		self.assertEqual(bs.alignments_in(2,1,board,ships),0)
 
 	#Check alignments on a whole board.
 	def test_whole_board_alignment(self):
@@ -54,7 +54,7 @@ class TestAlignments(unittest.TestCase):
 		[0,0,1,4]]
 		alignment_test_sum = 40
 
-		alignments, alignment_sum  = bs.possibleAlignments(board,ships)
+		alignments, alignment_sum  = bs.possible_alignments(board,ships)
 		for row, row_t in zip(alignments, alignments_test):
 			for a, a_t in zip(row,row_t):
 				self.assertEqual(a, a_t)
@@ -73,23 +73,27 @@ class TestTargeting(unittest.TestCase):
 		alignment_test_sum = 32
 
 		#Test alignments again
-		alignments, alignment_sum  = bs.possibleAlignments(board,ships)
+		alignments, alignment_sum  = bs.possible_alignments(board,ships)
 		for row, row_t in zip(alignments, alignments_test):
 			for a, a_t in zip(row,row_t):
 				self.assertEqual(a, a_t)
 		self.assertEqual(alignment_sum, alignment_test_sum)
 
 		#Test some impact values
-		impact_0_0 = bs.makeTarget(0,0,4,10) #If we shoot at (0,0)
-		impact_0_1 = bs.makeTarget(0,1,5,12) #If we shoot at (0,1)
-		impact_1_1 = bs.makeTarget(1,1,6,14) #If we shoot at (1,1)
+		impact_0_0 = bs.make_target(0,0,4,10) #If we shoot at (0,0)
+		impact_0_1 = bs.make_target(0,1,5,12) #If we shoot at (0,1)
+		impact_1_1 = bs.make_target(1,1,6,14) #If we shoot at (1,1)
 
-		targets = bs.possibleTargets(board, ships)
+		targets = bs.possible_targets(board, ships)
 
 		flag_0_0 = False
 		flag_0_1 = False
 		flag_1_1 = False
-		
+
+
+
+
+
 		for target in targets:
 			if impact_0_0 == target:
 				flag_0_0 = True
@@ -101,6 +105,69 @@ class TestTargeting(unittest.TestCase):
 		self.assertTrue(flag_0_0)
 		self.assertTrue(flag_0_1)
 		self.assertTrue(flag_1_1)
+
+class TestHitSelection(unittest.TestCase):
+
+	#Check whether it returns adjacent coordinates for single hits.
+	def test_adjacent_selection(self):
+		board = [['','','','',''],
+		['','H','','',''],
+		['L','','M','',''],
+		['','M','','',''],
+		['','M','','H','']]
+		ships = [2,3]
+		test_positions = [(0,1),(1,0),(1,2),(2,1),(3,3),(4,2),(4,4)]
+
+		hit_positions = bs.possible_hits(board, ships)
+		for tp in test_positions:
+			found = False
+			for hit_pos in hit_positions.keys():
+				if tp[0]==hit_pos[0] and tp[1]==hit_pos[1]:
+					found = True
+
+			#Checks that all the required finds are there.
+			self.assertTrue(found)
+			#Checks that there are no extra finds.
+			self.assertEqual(len(hit_positions),len(test_positions))
+
+	#Check whether it returns adjacent coordinates for longer hits and
+	#segments on the same column/row.
+	def test_adjacent_selection_long_segments(self):
+		board = [['','H','','',''],
+		['','H','','',''],
+		['L','','M','',''],
+		['H','M','H','H','H'],
+		['','M','','H','']]
+		ships = [2,3]
+		test_positions = [(0,0),(0,2),(1,0),(1,2),(2,1),(2,3),(2,4),(4,0),(4,2),(4,4)]
+		hit_positions = bs.possible_hits(board, ships)
+
+		for tp in test_positions:
+			found = False
+			for hit_pos in hit_positions:
+				if tp[0]==hit_pos[0] and tp[1]==hit_pos[1]:
+					found = True
+
+			#Checks that all the required finds are there.
+			self.assertTrue(found)
+			#Checks that there are no extra finds.
+			self.assertEqual(len(hit_positions),len(test_positions))
+
+	def test_adjacent_selection_long_segments(self):
+		board = [['','H','','',''],
+		['','H','','',''],
+		['L','','M','',''],
+		['H','M','H','H','H'],
+		['','M','','H','']]
+		ships = [2,3]
+		test_positions = [(0,0,1),(0,2,1),(1,0,1),(1,2,1),(2,1,2),(2,3,2),(2,4,1),(4,0,1),(4,2,2),(4,4,2)]
+		hit_positions = bs.possible_hits(board, ships)
+		for tp in test_positions:
+			found = False
+			for hit_pos in hit_positions:
+				if tp[0]==hit_pos[0] and tp[1]==hit_pos[1]:
+					self.assertEqual(tp[2],hit_positions[hit_pos])
+
 
 if __name__ == '__main__':
     unittest.main()
