c04d72c0e968fa96358513e96070fd06f1c87e75
NiklasZ <niklas.zwingenberger@gmail.com>
2017-12-23 19:48:25 +0100

    -Added pickled logging of game_states of games to allow re-use for training (#18) later.

--- project/src/utils/fileIO.py
+++ project/src/utils/fileIO.py
@@ -7,24 +7,41 @@ BOTS_DIR = '/bots'
 GAMES_DIR = '/games'
 OPP_DIR = '/opponents'
 PROFILE_FILE = 'profile.p'
+GAMES_LOG_FILE = 'log.p'
+
 
 # Load a bot's opponent profile. Returns a dict if it exists.
 def load_profile(bot_name, opponent_name):
     profile_path = DATA_DIR + BOTS_DIR + '/' + bot_name + OPP_DIR + '/' + opponent_name + '/profile.p'
-    if os.path.exists(profile_path):
-        return pickle.load(open(profile_path, "rb" ))
-    else:
-        return None
+    load_pickle_if_exists(profile_path)
+
 
 # Store a bot's opponent profile.
 def save_profile(profile, bot_name, opponent_name):
     profile_dir = DATA_DIR + BOTS_DIR + '/' + bot_name + OPP_DIR + '/' + opponent_name + '/profile.p'
     create_dirs(bot_name, opponent_name)
-    pickle.dump(profile, open(profile_dir,'wb'))
+    pickle.dump(profile, open(profile_dir, 'wb'))
 
-def save_game_log(bot_name, opponent_name, game_id,log):
-    game_path = DATA_DIR + BOTS_DIR + '/' + bot_name + OPP_DIR + '/' + opponent_name + GAMES_DIR + '/'+str(game_id)
-    write_to_file(log, game_path)
+
+def save_game_log(bot_name, opponent_name, game_id, log, pickled_log):
+    game_log_path = DATA_DIR + BOTS_DIR + '/' + bot_name + OPP_DIR + '/' + opponent_name + GAMES_DIR + '/' + str(
+        game_id)
+    game_pickled_log_path = DATA_DIR + BOTS_DIR + '/' + bot_name + OPP_DIR + '/' + opponent_name + GAMES_DIR + '/' + GAMES_LOG_FILE
+    write_to_file(log, game_log_path)
+    pickle.dump(pickled_log, open(game_pickled_log_path, 'wb'))
+
+
+def load_pickled_game_log(bot_name, opponent_name):
+    game_log_path = DATA_DIR + BOTS_DIR + '/' + bot_name + OPP_DIR + '/' + opponent_name + GAMES_DIR + '/' + GAMES_LOG_FILE
+    load_pickle_if_exists(game_log_path)
+
+
+def load_pickle_if_exists(path):
+    if os.path.exists(path):
+        return pickle.load(open(path, "rb"))
+    else:
+        return None
+
 
 def read_file(file_name):
     with open(file_name, mode='r') as reader:
@@ -38,8 +55,8 @@ def write_to_file(text, file_name):
 
 # Creates directories for bot and opponent.
 def create_dirs(bot_name, opponent_name):
-    bot_game_dir = DATA_DIR+BOTS_DIR+'/'+bot_name+OPP_DIR+'/'+opponent_name+GAMES_DIR
-    img_dir = DATA_DIR+IMG_DIR
+    bot_game_dir = DATA_DIR + BOTS_DIR + '/' + bot_name + OPP_DIR + '/' + opponent_name + GAMES_DIR
+    img_dir = DATA_DIR + IMG_DIR
     make_dir(bot_game_dir)
     make_dir(img_dir)
 
--- project/src/utils/game_recorder.py
+++ project/src/utils/game_recorder.py
@@ -7,17 +7,23 @@ class GameRecorder:
     def __init__(self, game_state, bot_name):
         self.bot_name = bot_name  # Name of our bot.
         self.opponent_name = game_state['OpponentId']  # Name of opponent's bot.
-        self.gameId = game_state['GameId']  # ID of the game.
+        self.game_id = game_state['GameId']  # ID of the game.
+        self.game_history = io.load_pickled_game_log(self.bot_name, self.opponent_name)
+        if not self.game_history:
+            self.game_history = {}
+        self.game_states = [game_state]
 
         # Initial setup.
         io.create_dirs(self.bot_name, self.opponent_name)
         self.log = [datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S')]
-        self.log.append('\nGameId: ' + str(self.gameId))
+        self.log.append('\nGameId: ' + str(self.game_id))
         self.log.append('\nShips: ' + str(game_state['Ships']))
         self.log.append('\n--GAME START--\n\n')
 
     # Record both boards every turn.
     def record_turn(self, game_state):
+        self.game_states.append(game_state)
+
         self.log.append('\n')
         self.log.append(self.bot_name)
         self.log.append('\n' + self.board_to_string(game_state['MyBoard']) + '\n')
@@ -28,7 +34,8 @@ class GameRecorder:
     # Wrap up and write the file.
     def record_end(self):
         self.log.append('\n\n--GAME END--')
-        io.save_game_log(self.bot_name, self.opponent_name, self.gameId, ''.join(self.log))
+        self.game_history[self.game_id] = self.game_states
+        io.save_game_log(self.bot_name, self.opponent_name, self.game_id, ''.join(self.log), self.game_history)
 
     # Converts board to somewhat more readable string.
     def board_to_string(self, board):
